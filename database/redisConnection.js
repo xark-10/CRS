39376414c619bb822742deee0acbb31d9aa42be5
xark-10 <84316716+xark-10@users.noreply.github.com>
2022-03-29 14:56:05 +0530

    final

--- database/redisConnection.js
+++ database/redisConnection.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 
 //connection to redis 
-const redisClient = redis.createClient({host: process.env.REDIS_HOST,port: process.env.REDIS_PORT});
+const redisClient = redis.createClient("crs.tr6gd5.0001.use1.cache.amazonaws.com:6379");
 
 redisClient.on('connect',function(){
     console.log(`Redis Client Connected at port ${process.env.REDIS_PORT}`)
