b046149ec2093cfb3643e23f6d5e8d554a7fb084
xark-10 <84316716+xark-10@users.noreply.github.com>
2022-03-29 13:34:47 +0530

    final

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ubuntu/express-app
+sudo chmod -R 777 /home/ec2-user/express-app
 
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/express-app
